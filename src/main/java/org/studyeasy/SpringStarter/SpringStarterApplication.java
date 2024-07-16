fb01a90beabc597a2bba2cb5012c7cd4ac251a00
allensftort <allenstfort1@gmail.com>
2024-07-15 23:44:23 -0400

    Spring project

---
+++ src/main/java/org/studyeasy/SpringStarter/SpringStarterApplication.java
@@ -0,0 +1,16 @@
+package org.studyeasy.SpringStarter;
+
+import java.io.Console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterApplication.class, args);
+
+	}
+
+}
